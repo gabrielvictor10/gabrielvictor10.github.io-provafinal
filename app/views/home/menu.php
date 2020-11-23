7081e0e03f3b9bf803f328cde2f429ee823f6a45
gabrielvictor10 <gabrielvictor10sv@gmail.com>
2020-11-23 13:05:00 -0300

    1

---
+++ app/views/home/menu.php
@@ -0,0 +1,6 @@
+            <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Sobre mim</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Hobbies</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Afazeres</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contatos</a></li>
+            </ul>
